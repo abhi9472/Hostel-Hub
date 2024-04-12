4f38a1653b909ab067eb1f41871136f7f2961784
Abhishek <abhishekkumarcse2015@gmail.com>
2024-04-13 01:33:49 +0530

    Indication added

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -9,8 +9,8 @@ function SignUp() {
     gender: '',
     email: '',
     phoneNum: '',
-    hostel_name:'',
-    password:'',
+    hostel_name: '',
+    password: '',
     avatar: null
   });
 
@@ -47,139 +47,147 @@ function SignUp() {
       }
 
       // Handle successful signup, e.g., show success message or redirect
-      
+
       localStorage.setItem('User', response.data.data._id); // Store access token in localStorage
 
       window.location.href = '/verify-otp';
-       
+
       console.log('Signup successful');
 
-       //window.location.href = '/login'
+      //window.location.href = '/login'
     } catch (error) {
       console.error('Error signing up:', error.message);
       // Handle signup error, e.g., display error message to the user
     }
   };
-  
+
 
   return (
     <div className="max-w-lg mx-auto mt-8 p-6 bg-white rounded shadow-md">
-            <h2 className="text-2xl mb-4">Signup</h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">Username:</label>
-                    <input
-                        type="text"
-                        name="username"
-                        value={formData.username}
-                        onChange={handleChange}
-                        required
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    />
-                </div>
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">Name:</label>
-                    <input
-                        type="text"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    />
-                </div>
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">UID:</label>
-                    <input
-                        type="text"
-                        name="uid"
-                        value={formData.uid}
-                        onChange={handleChange}
-                        required
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    />
-                </div>
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">Gender:</label>
-                    <select
-                        name="gender"
-                        value={formData.gender}
-                        onChange={handleChange}
-                        required
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    >
-                        <option value="">Select Gender</option>
-                        <option value="MALE">MALE</option>
-                        <option value="FEMALE">FEMALE</option>
-                        <option value="OTHER">OTHER</option>
-                    </select>
-                </div>
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">Email-Official:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    />
-                </div>
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">Password-Len(8):</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        required
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    />
-                </div>
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">Phone Number:</label>
-                    <input
-                        type="tel"
-                        name="phoneNum"
-                        value={formData.phoneNum}
-                        onChange={handleChange}
-                        required
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    />
-                </div>
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">Hostel:</label>
-                    <select
-                        name="hostel_name"
-                        value={formData.hostel_name}
-                        onChange={handleChange}
-                        required
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    >
-                        <option value="">Select Hostel</option>
-                        <option value="NekChand/Zakir">NekChand/Zakir</option>
-                        <option value="SUKHNA">SUKHNA</option>
-                        <option value="TAGORE">TAGORE</option>
-                    </select>
-                </div>
-                <div>
-                    <label className="block mb-1 font-medium text-gray-700">Profile Picture:</label>
-                    <input
-                        type="file"
-                        name="avatar"
-                        accept="image/*"
-                        onChange={handleProfilePicChange}
-                        className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-                >
-                    Sign Up
-                </button>
-            </form>
+      <h2 className="text-2xl mb-4">Signup</h2>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+            minLength={5}
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          />
         </div>
-    );
-}  
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+            minLength={1}
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">UID:</label>
+          <input
+            type="text"
+            name="uid"
+            value={formData.uid}
+            onChange={handleChange}
+            required
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">Gender:</label>
+          <select
+            name="gender"
+            value={formData.gender}
+            onChange={handleChange}
+            required
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          >
+            <option value="">Select Gender</option>
+            <option value="MALE">MALE</option>
+            <option value="FEMALE">FEMALE</option>
+            <option value="OTHER">OTHER</option>
+          </select>
+        </div>
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">Email-CU_Official:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            pattern="[a-zA-Z0-9._%+-]+@(cuchd\.in|cumail\.in)$"
+            required
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          />
+
+
+        </div>
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">Password-Len(8):</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+            minLength={8}
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">Phone Number:</label>
+          <input
+            type="tel"
+            name="phoneNum"
+            value={formData.phoneNum}
+            onChange={handleChange}
+            required
+            minLength={10}
+            maxLength={10}
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">Hostel:</label>
+          <select
+            name="hostel_name"
+            value={formData.hostel_name}
+            onChange={handleChange}
+            required
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          >
+            <option value="">Select Hostel</option>
+            <option value="NekChand/Zakir">NekChand/Zakir</option>
+            <option value="SUKHNA">SUKHNA</option>
+            <option value="TAGORE">TAGORE</option>
+          </select>
+        </div>
+        <div>
+          <label className="block mb-1 font-medium text-gray-700">Profile Picture || Optional:</label>
+          <input
+            type="file"
+            name="avatar"
+            accept="image/*"
+            onChange={handleProfilePicChange}
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}
 export default SignUp;
