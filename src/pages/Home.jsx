47c5aad5d900b43955a34fce019c2afc9642d482
Abhishek <abhishekkumarcse2015@gmail.com>
2024-04-18 02:11:11 +0530

    Home Page: Product rendering fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
         throw new Error('Failed to fetch slider products');
       }
       const responseData = await response.json();
-      console.log(responseData);
+      // console.log(responseData);
       const sliderProducts = responseData.data || [];
       setProducts(sliderProducts);
     } catch (error) {
@@ -35,11 +35,12 @@ function Home() {
   const fetchProducts = async () => {
     try {
       const response = await fetch('https://hostelhub-backend.onrender.com/api/v1/product/allProducts');
-      // console.log(response);
       if (!response.ok) {
         throw new Error('Failed to fetch products');
       }
       const responseData = await response.json();
+      // console.log(responseData);
+
       const productData = responseData.data || [];
       setProducts(productData);
     } catch (error) {
@@ -109,41 +110,41 @@ function Home() {
               </div>
             ))}
           </Carousel>
-      </div>
-      <div className="container mx-auto px-4 py-8">
-        <h2 className="text-3xl font-semibold mb-6">New Products</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {products.map((product) => (
-            <div
-              key={product._id}
-              className="relative bg-white rounded-lg shadow-md overflow-hidden"
-            >
-              {/* Wrap the product name inside a Link component */}
-              <Link to={`/product/${product._id}`}>
-                <img
-                  src={product.coverImg}
-                  alt={product.name}
-                  className="w-full h-48 object-cover object-center cursor-pointer"
-                />
-              </Link>
-              <div className="p-4">
-                {/* Also wrap the product name inside the Link component */}
-                <Link to={`/product/${product._id}`}>
-                  <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
-                </Link>
-                <p className="text-gray-600 mb-4">{product.description}</p>
-                <p className="text-lg font-semibold text-blue-500">
-                  Price: ₹ {product.price}
-                </p>
-              </div>
-            </div>
-          ))}
         </div>
+        <div className="container mx-auto px-4 py-8">
+          <h2 className="text-3xl font-semibold mb-6">New Products</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {products.length > 0 && products.map((product) => (
+              <div
+                key={product._id}
+                className="relative bg-white rounded-lg shadow-md overflow-hidden"
+              >
+                {/* Wrap the product name inside a Link component */}
+                <Link to={`/product/${product._id}`}>
+                  <img
+                    src={product.coverImg}
+                    alt={product.name}
+                    className="w-full h-48 object-cover object-center cursor-pointer"
+                  />
+                </Link>
+                <div className="p-4">
+                  {/* Also wrap the product name inside the Link component */}
+                  <Link to={`/product/${product._id}`}>
+                    <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
+                  </Link>
+                  <p className="text-gray-600 mb-4">{product.description}</p>
+                  <p className="text-lg font-semibold text-blue-500">
+                    Price: ₹ {product.price}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+
       </div>
-
-
-
-    </div>
     </div>
   )
 }
